5aab77dd4f07e22c76c526998c2376e073eb6c97
Adolfo Herrera <valherdolf@gmail.com>
2023-08-18 21:27:19 -0400

    feat: Add array containing boilerplate message objects, render 'index' and 'new' pages, push new message upon post request to 'new' page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date(),
+    },
+    {
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date(),
+    },
+];
+
+/* GET /home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+/* GET /new page. */
+router.get('/new', (req, res, next) => {
+    res.render('form');
+});
+
+/* POST new message */
+router.post('/new', (req, res, next) => {
+    const { messageUser, messageText } = req.body;
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    res.redirect('/');
 });
 
 module.exports = router;
